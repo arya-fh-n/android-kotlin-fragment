2f653ba161d4fd5d1176bb493571adc22b99b94f
wiryadev <abrarwiryawan@gmail.com>
2022-09-13 22:16:05 +0700

    optimize import

--- app/src/main/java/com/arfdevs/fragmentpractice/CategoryFragment.kt
+++ app/src/main/java/com/arfdevs/fragmentpractice/CategoryFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 
 class CategoryFragment : Fragment(), View.OnClickListener {
